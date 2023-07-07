380f94330c22ee9f517b8fe422c084d099bad9e0
Uthaeus <romanlavery@gmail.com>
2023-07-06 21:21:01 -0600

    created home and about components, created stack navigator routing in app

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomePage from './screens/HomePage';
+import AboutPage from './screens/AboutPage';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen name="About" component={AboutPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
